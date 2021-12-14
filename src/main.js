7fa2c20f95eb91abe53849bc1c7725c67fbdbbf1
Mario <lombardomario01@gmail.com>
2021-12-14 16:06:57 +0100

    Bootstrap package installed

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faChevronUp, faChevronRight, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF, faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 library.add(faChevronUp, faChevronRight, faFacebookF, faTwitter, faYoutube, faInstagram, faMapMarkerAlt, faPhone)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
